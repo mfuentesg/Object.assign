33bbbb976ca12c97e97cdbce42432748f8f4422a
Marcelo Fuentes <marceloe.fuentes@gmail.com>
2016-10-31 19:43:45 -0300

    Add browser, amd and commonjs support

--- assign.js
+++ assign.js
@@ -1,31 +1,50 @@
-module.exports.assign = function() {
-  var argType, isArray, arg, merge;
-  var args = arguments;
-
-  if (args.length < 1) {
-    throw new TypeError('Cannot convert undefined or null to object');
+!(function(root, moduleName, factory) {
+  if (typeof module === 'object' && module.exports) {
+    // CommonJS support
+    module.exports = factory(require(moduleName));
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD support
+    define([moduleName], factory);
+  } else {
+    // Browser support
+    root[moduleName] = factory(root[moduleName]);
   }
+}(this, 'assign', function assign() {
+  return {
+    assign: function() {
+      var argType;
+      var isArray;
+      var arg;
+      var merge;
+      var args = arguments;
 
-  for (arg in args) {
-    arg = args[arg];
-    argType = typeof(arg);
-    isArray = Array.isArray(arg);
+      if (args.length < 1) {
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
 
-    if (argType !== 'object' || isArray) {
-      argType = isArray ? 'array' : argType;
-      throw new TypeError('Cannot convert ' + argType + ' to object');
+      for (arg in args) {
+        arg = args[arg];
+        argType = typeof(arg);
+        isArray = Array.isArray(arg);
+
+        if (argType !== 'object' || isArray) {
+          argType = isArray ? 'array' : argType;
+          throw new TypeError('Cannot convert ' + argType + ' to object');
+        }
+      }
+
+      merge = function(baseObject, newObject) {
+        Object.keys(newObject).forEach(function(updateKey) {
+          baseObject[updateKey] = newObject[updateKey];
+        });
+
+        return baseObject;
+      };
+
+      return Object.keys(args).reduce(function(baseValue, currentKey) {
+        return merge(baseValue, args[currentKey]);
+      }, {});
     }
   }
+}));
 
-  merge = function(baseObject, newObject) {
-    Object.keys(newObject).forEach(function(updateKey) {
-      baseObject[updateKey] = newObject[updateKey];
-    });
-
-    return baseObject;
-  };
-
-  return Object.keys(args).reduce(function(baseValue, currentKey) {
-    return merge(baseValue, args[currentKey]);
-  }, {});
-};
